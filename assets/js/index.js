5ed7479e45afc587ba030d29b329d28c206e57d1
Davej66 <daijacobs@gmail.com>
2021-10-08 17:48:45 +0000

    Adding places detail

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,3 +74,25 @@ function initAutocomplete() {
     });
   }
   
+
+
+  /**-------------------------------placed js */
+
+  let map;
+
+function initMap() {
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: [
+      { type: "restaurant" },
+      { type: "tourist_attraction" },
+    ],
+    maxPlaceCount: 10,
+  });
+
+  map = localContextMapView.map;
+  map.setOptions({
+    center: { lat: 51.507307, lng: -0.08114 },
+    zoom: 14,
+  });
+}
